7ef3210328a6d7ccc49ce6317bd87c48804e4e73
Julián <julianmmame@gmail.com>
2020-08-08 15:23:37 -0300

    trabajando sobre la iteración de secciones

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+'use strict'
+
+$(document).ready(function(){
+
+    var OnOneClick = function() { 
+        // Your click handler
+      };
+      
+      var OneClick = document.getElementsById("#one");
+      OneClick.addEventListener('click', OnOneClick, false);
+
+});
