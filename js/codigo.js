8fef908aafbbd3353aefa4b708b0db3a74c04580
Julián <julianmmame@gmail.com>
2020-08-08 16:29:00 -0300

    trabajando sobre alternar estaciones

--- js/codigo.js
+++ js/codigo.js
@@ -1,12 +1,14 @@
 'use strict'
 
-$(document).ready(function(){
-
-    var OnOneClick = function() { 
-        // Your click handler
-      };
-      
-      var OneClick = document.getElementsById("#one");
-      OneClick.addEventListener('click', OnOneClick, false);
-
-});
+var OneClick = document.getElementById('next-titulo-estacion-3d');
+//console.log(OneClick);
+OneClick.addEventListener('click', function () {
+    console.log('paso a estación 2');
+    document.getElementById('estacion3d').className = 'hidden estacion';
+    document.getElementById('estacionPostprod').className = 'estacion';
+    document.getElementById('body').className = 'aecolorRojo';
+/*
+    var OneClick = document.getElementsById('next-titulo-estacion-3d');
+    OneClick.addEventListener('click', OnOneClick, false);
+*/
+}, false);
